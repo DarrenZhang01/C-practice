f2895038b8bc2003ea8948855b1a36c044ab2584
Darren <18633059886@163.com>
2018-05-15 13:00:00 -0400

    File System 3

---
+++ File3/read2.c
@@ -0,0 +1,24 @@
+/* Read the string of the file by line */
+
+#include <stdio.h>
+int main ()
+{
+    FILE * fp;
+    char w[81], name[30], *filename = name;
+    int lines = 0;
+    printf("Please input the file name:\n");
+    gets(name);
+    fp = fopen(filename, "r");
+    if (fp == NULL)
+        printf("File open error!\n");
+    else
+    {
+        while (fgets(w, 80, fp) != NULL)
+        {
+            lines = lines + 1;
+            printf("***%s", w);
+        }
+        printf("The total lines of the file=%d\n", lines);
+        fclose(fp);
+    }
+}
